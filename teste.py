4b0f34a8de51cc55dcd2d8049e0895659d40a52a
Luan Luz <luandonato@protonmail.com>
2025-02-09 13:19:55 -0300

    class att

--- teste.py
+++ teste.py
@@ -1,26 +1,15 @@
 from flask import Flask, Blueprint, request, jsonify
 from classeproduto import Product, check_disponibility
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-produtos = [
-    {
-        'id': 1,
-        'nome' = 'Coca-Cola',
-        'descricao' = 'Refrigerante de cola',
-        'preco' = 5.00
-        'disponibilidade' = True
-    },
-]
-
-@server.route("/")
+@app.route("/")
 def home():
     return "Olá, Zina!"
 
-@server.route("/cadastro", methods=['POST'])
+@app.route("/cadastro", methods=['POST', 'GET'])
 def cadastro():
-    data = request.json
-    return data['descricao']
+    return jsonify(products)
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    app.run(port=5000, host='localhost', debug=True)
